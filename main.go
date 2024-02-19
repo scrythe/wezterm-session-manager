6c5ad2a67fc3621efdfd9671a7d6a896fc01648a
Magomed Alimkhanov <scrythepvp@gmail.com>
2024-02-19 15:20:16 +0100

    feat: add text highlichting and improve styling

--- main.go
+++ main.go
@@ -9,54 +9,57 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-type searchFieldStyle struct {
-	BorderColor lipgloss.Color
-	InputField  lipgloss.Style
+type Styles struct {
+	BorderColor       lipgloss.Color
+	TextColor         lipgloss.Color
+	TextHiglightColor lipgloss.Color
+
+	InputField lipgloss.Style
+	TextField  lipgloss.Style
 }
 
-type highlightStyle struct {
-	TextColor lipgloss.Color
-	TextField lipgloss.Style
+func (s Styles) Highlight(text string) string {
+	s.TextField.Foreground(s.TextHiglightColor)
+	highLightText := s.TextField.Render(text)
+	s.TextField.Foreground(s.TextColor)
+	return highLightText
 }
 
-func DefaultStyles() (*searchFieldStyle, *highlightStyle) {
-	s := new(searchFieldStyle)
-	t := new(highlightStyle)
+func DefaultStyles() *Styles {
+	s := new(Styles)
 	s.BorderColor = lipgloss.Color("36")
-	t.TextColor = lipgloss.Color("32")
+	s.TextColor = lipgloss.Color("32")
+	s.TextHiglightColor = lipgloss.Color("36")
 
 	s.InputField = lipgloss.
 		NewStyle().
 		BorderForeground(s.BorderColor).
 		BorderStyle(lipgloss.NormalBorder()).
-		// Padding(1).
 		Width(80).
-		Foreground(t.TextColor)
-	t.TextField = lipgloss.NewStyle().
-		Foreground(s.BorderColor)
+		Foreground(s.TextColor)
+	s.TextField = lipgloss.NewStyle().
+		Foreground(s.TextColor)
 
-	return s, t
+	return s
 }
 
 type model struct {
-	items            []string
-	searchField      textinput.Model
-	searchFieldStyle *searchFieldStyle
-	highlightStyle   *highlightStyle
-	width            int
-	height           int
+	items       []string
+	searchField textinput.Model
+	styles      *Styles
+	width       int
+	height      int
 }
 
 func New(items []string) *model {
-	searchFieldStyle, highlightStyle := DefaultStyles()
+	styles := DefaultStyles()
 	inputField := textinput.New()
 	inputField.Placeholder = "Search Project"
 	inputField.Focus()
 	return &model{
-		items:            items,
-		searchField:      inputField,
-		searchFieldStyle: searchFieldStyle,
-		highlightStyle:   highlightStyle,
+		items:       items,
+		searchField: inputField,
+		styles:      styles,
 	}
 }
 
@@ -85,7 +88,7 @@ func (m model) View() string {
 	if m.width == 0 {
 		return "loading.."
 	}
-
+	m.items = append(m.items, m.styles.Highlight("testhghmmm"))
 	return lipgloss.Place(
 		m.width,
 		m.height,
@@ -93,8 +96,13 @@ func (m model) View() string {
 		lipgloss.Center,
 		lipgloss.JoinVertical(
 			0.05,
-			m.searchFieldStyle.InputField.Render(m.searchField.View()),
-			m.highlightStyle.TextField.Render(lipgloss.JoinVertical(lipgloss.Left, m.items...)),
+			m.styles.InputField.Render(m.searchField.View()),
+			m.styles.TextField.Render(
+				lipgloss.JoinVertical(
+					lipgloss.Left,
+					m.items...,
+				),
+			),
 		),
 	)
 }
